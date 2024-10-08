e4659d6ab5b165d5ce3d72db8be779c085d9161d
Vincent Truong <vincenttruong6050@gmail.com>
2024-10-07 17:17:05 -0700

    files from visualstudio

---
+++ visualstudio/ProductionManager.cpp
@@ -0,0 +1,68 @@
+//The actual ProductionManager.cpp
+#include "ProductionManager.h"
+
+///////////////////////////////////////////////
+
+//Constructor
+/*
+`ProductionManager` has a list of workers who are reserved to build buildings.
+ @m_reservedWorkers - list of workers that are currently idle
+*/
+ProductionManager::ProductionManager(unitManager& manager) : unitManager(manager) {
+	m_reservedWorkers();
+}
+
+///////////////////////////////////////////////
+
+
+//addBuildRequest
+/*
+When the `addBuildRequest()` function is called, the manager will attempt to
+reserve a new worker and tell the worker to build the appropriate building. If
+no worker could be reserved, the function returns false.
+*/
+bool addBuildRequests(bw::UnitType buildingType) {
+	//TODO:meaning of this
+}
+
+
+//countBuildRequests
+/*
+The `countBuildRequests()` function counts how many buildings of the specified type
+are being built by workers. (in progress builds)
+*/
+int countBuildRequests(bw::UnitType buildingType) {
+	//TODO:meaning of this
+	// How many workers are working on one building?
+	//uses number of buildings and add
+}
+
+
+//countBuildings
+/*
+The `countBuildings()` function counts how many
+buildings of the specified type have been completed.
+*/
+int countBuildings(bw::UnitType buildingType) {
+	int numOfBuildings = Tools::CountUnitsOfType(buildingType, g_self->getUnits());
+	return numOfBuildings;
+}
+
+
+////////////////////////////////////////////////
+
+
+void ProductionManager::onStart() {
+	m_allUnits.clear();
+	m_freeUnits.clear();
+}
+
+void ProductionManager::onUnitDestroy(bw::Unit unit) {
+	m_allUnits.erase(unit);
+	m_freeUnits.erase(unit);
+}
+
+void ProductionManager::onUnitComplete(bw::Unit unit) {
+	m_allUnits.erase(unit);
+	m_freeUnits.erase(unit)
+}
