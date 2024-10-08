aecaedbad26ce8d00c52cf00037784e1b18cc5b0
Vincent Truong <vincenttruong6050@gmail.com>
2024-10-07 17:10:46 -0700

    currently working on production-manager-class

---
+++ src/starterbot/ProductionManager.h
@@ -0,0 +1,35 @@
+#pragam once
+
+#include "./Tools.h"
+
+//The actual ProductionManager.h
+
+//good use of #include <climits> e.g. INT_MAX
+
+class ProductionManager : public EventReceiver {
+private:
+	bw::Unitset m_allUnits;
+	bw::Unitset m_freeUnits;
+	UnitManager& unitManager; //store a reference to UnitManager(?)
+	bw::Unitset m_reservedWorkers; //a set of reserved workers
+
+
+public:
+	//what are the matchUnit/matchUnits/matchCount coming from?
+	//what are peekUnit/peekUnits/peekCount coming from?
+	
+
+	//ProductionManager(UnitManager& manager)
+	ProductionManager(UnitManager& manager);
+
+	bool addBuildRequest(bw::UnitType);
+	int countBuildRequests(bw::UnitType);
+	int countBuildings(bw::UnitType);
+
+
+protected:
+	virtual void onStart() override;
+	virtual void onFrame() override;
+	virtual void onUnitDestroy(bw::Unit unit) override;
+
+};
