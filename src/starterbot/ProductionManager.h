29a0e9ae448cc23a290a7595374f44ec9713db64
Vincent Robinson <robinsonvincent89@gmail.com>
2025-03-19 16:46:09 -0700

    Implement continuous production and expansion (#15)
    Co-authored-by: Vincent Robinson <robinsma25@up.edu>

--- src/starterbot/ProductionManager.h
+++ src/starterbot/ProductionManager.h
@@ -3,15 +3,21 @@
 #include "Tools.h"
 #include "UnitManager.h"
 
-// This class is in charge of handling what each of the bot's workers is doing. It handles
-// the gathering of resources and the construction of new buildings.
+// This class is in charge of handling what each of the bot's workers and buildings are
+// doing. It handles the gathering of resources, the training/morphing of units, and the
+// construction of new buildings.
 class ProductionManager : public EventReceiver {
 private:
     UnitManager& m_unitManager;
 
-    // The set of workers that are reserved to build a building. Workers that are
-    // gathering minerals are not reserved to keep them available for other managers.
-    bw::Unitset m_workers;
+    // The set of workers that are reserved to build a building or morph into another
+    // building. Workers that are gathering minerals are not reserved to keep them
+    // available for other managers.
+    bw::Unitset m_builders;
+
+    // The set of all buildings that are currently reserved to train some units as well as
+    // units that are morphing into a non-building type.
+    bw::Unitset m_trainers;
 
 public:
     ProductionManager(UnitManager& unitManager);
@@ -20,15 +26,31 @@ public:
     // resources, a place to put the building, and a worker to construct it, then this
     // function returns true. Otherwise, the request fails.
     bool addBuildRequest(bw::UnitType type);
-
     // Counts the number of pending building requests, meaning that a worker is assigned
     // to construct the building but no building has been placed yet.
     int countBuildRequests(bw::UnitType type);
 
-    // For Zerg units, requests that the unit morphs into another unit. Buildings still
-    // require a build command rather than a morph command, even though both appear to be
-    // morphing from the user's point of view.
-    bool addMorphRequest(bw::UnitType type);
+    // Requests that a certain unit type be trained or morphed. Returns true if a building
+    // or morph source of the appropriate type was available and sufficient resources are
+    // available. Otherwise, the request fails.
+    bool addTrainRequest(bw::UnitType type, bool morph);
+
+    // Requests that buildings of a certain type be built up to a certain quota. If the
+    // quota is already fulfilled, then this does nothing. Otherwise, it tries to add
+    // enough build requests to meet the quota. Returns true if it was able to do so.
+    bool targetBuildRequests(bw::UnitType type, int count);
+    // Requests that units of a certain type be trained or morphed up to a certain quota,
+    // just like targetBuildRequests().
+    bool targetTrainRequests(bw::UnitType type, int count, bool morph);
+
+    // Adds train requests to all relevant idle buildings or morphable units to train
+    // units of a particular type.
+    void idleTrainRequests(bw::UnitType type, bool morph);
+    // Adds train requests to all relevant idle buildings or morphable units to train one
+    // of many different units. The unit to train is selected according to a probability
+    // value between 0 and 1 for each unit. All units in the list must be trainable from
+    // the same basic building or morphable unit.
+    void groupTrainRequests(const std::vector<std::pair<bw::UnitType, double>>& items, bool morph);
 
 protected:
     virtual void onStart() override;
--- src/starterbot/StrategyManager.cpp
+++ src/starterbot/StrategyManager.cpp
@@ -2,7 +2,6 @@
 
 StrategyManager::StrategyManager() :
     m_productionManager(m_unitManager),
-    m_buildingManager(m_unitManager),
     m_scoutManager(m_unitManager),
     m_combatManager(m_unitManager) {
 }
@@ -11,137 +10,101 @@ void StrategyManager::notifyMembers(const bw::Event& event) {
     m_unitManager.notifyReceiver(event);
 
     m_productionManager.notifyReceiver(event);
-    m_buildingManager.notifyReceiver(event);
     m_scoutManager.notifyReceiver(event);
     m_combatManager.notifyReceiver(event);
 }
 
-void StrategyManager::onStart() {
-    // For now, we only have one predefined build order per race, but in the future we can
-    // make the choice of build order take into account the race of our opponent.
+void StrategyManager::onFrame() {
     if (g_self->getRace() == bw::Races::Protoss) {
-        m_strategy = {
-            {ActionType::TRAIN,   /* 0 */   -1,  bw::UnitTypes::Protoss_Probe,        7 },
-            {ActionType::BUILD,   /* 1 */   0,   bw::UnitTypes::Protoss_Pylon,        1 },
-            {ActionType::SCOUT,   /* 2 */   1,   bw::UnitTypes::Protoss_Probe,        1 },
-            {ActionType::TRAIN,   /* 3 */   2,   bw::UnitTypes::Protoss_Probe,        13},
-            {ActionType::BUILD,   /* 4 */   2,   bw::UnitTypes::Protoss_Gateway,      1 },
-            {ActionType::TRAIN,   /* 5 */   4,   bw::UnitTypes::Protoss_Zealot,       2 },
-            {ActionType::ATTACK,  /* 6 */   5,                                          },
-        };
+        onProtossFrame();
     } else if (g_self->getRace() == bw::Races::Terran) {
-        m_strategy = {
-            {ActionType::TRAIN,   /* 0 */   -1,  bw::UnitTypes::Terran_SCV,           8 },
-            {ActionType::BUILD,   /* 1 */   0,   bw::UnitTypes::Terran_Supply_Depot,  1 },
-            {ActionType::SCOUT,   /* 2 */   1,   bw::UnitTypes::Terran_SCV,           1 },
-            {ActionType::TRAIN,   /* 3 */   2,   bw::UnitTypes::Terran_SCV,           16},
-            {ActionType::BUILD,   /* 4 */   2,   bw::UnitTypes::Terran_Barracks,      1 },
-            {ActionType::TRAIN,   /* 5 */   4,   bw::UnitTypes::Terran_Marine,        2 },
-            {ActionType::ATTACK,  /* 6 */   5,                                          },
-        };
+        onTerranFrame();
     } else {
-        m_strategy = {
-            {ActionType::MORPH,   /* 0 */   -1,  bw::UnitTypes::Zerg_Drone,           9 },
-            {ActionType::BUILD,   /* 1 */   0,   bw::UnitTypes::Zerg_Spawning_Pool,   1 },
-            {ActionType::SCOUT,   /* 2 */   1,   bw::UnitTypes::Zerg_Drone,           1 },
-            {ActionType::MORPH,   /* 3 */   1,   bw::UnitTypes::Zerg_Overlord,        2 },
-            {ActionType::MORPH,   /* 4 */   3,   bw::UnitTypes::Zerg_Drone,           13},
-            {ActionType::MORPH,   /* 5 */   1,   bw::UnitTypes::Zerg_Zergling,        2 },
-            {ActionType::ATTACK,  /* 6 */   5,                                          },
-        };
+        onZergFrame();
     }
-
-    // Make sure we reset the completion vector to all false values and resize it to be
-    // the same size as the strategy vector.
-    m_completion.clear();
-    m_completion.resize(m_strategy.size());
 }
 
-void StrategyManager::onFrame() {
-    // Loop through every item in the build order and see whether we need to execute it.
-    for (int i = 0; i < m_strategy.size(); i++) {
-        const ActionItem& item = m_strategy[i];
+void StrategyManager::onProtossFrame() {
+    // First, we make sure none of our buildings are idle by training new units.
+    m_productionManager.idleTrainRequests(bw::UnitTypes::Protoss_Probe, false);
+    m_productionManager.idleTrainRequests(bw::UnitTypes::Protoss_Zealot, false);
 
-        // If this item's dependency (if any) hasn't been completed, then do nothing here.
-        if (item.depends != -1 && !m_completion[item.depends]) {
-            continue;
-        }
+    // Make sure we have enough pylons to support the number of units we have. Since
+    // Protoss units are relatively slow to make, we don't need to keep as much slack in
+    // our supply amount as the other races.
+    m_productionManager.targetBuildRequests(
+        bw::UnitTypes::Protoss_Pylon, g_self->supplyUsed() / 14);
 
-        // Otherwise, we need to delegate an action to the appropriate manager class.
-        switch (item.action) {
-        case ActionType::BUILD:
-        {
-            // If the current number of buildings (including buildings that are only
-            // partially constructed) meets the quota, then we're done.
-            int current = m_unitManager.selfCount(bw::Filter::GetType == item.type);
-            int progress = current + m_productionManager.countBuildRequests(item.type);
+    int workerCount = m_unitManager.selfCount(bw::GetType == bw::UnitTypes::Protoss_Probe);
+    int fighterCount = m_unitManager.selfCount(bw::GetType == bw::UnitTypes::Protoss_Zealot);
 
-            m_completion[i] = current >= item.count;
+    // If we have enough workers with which to gather resources, we can send one of them
+    // out to go do some scouting.
+    if (workerCount >= 8 && m_scoutManager.countScouts(bw::UnitTypes::Protoss_Probe) == 0) {
+        m_scoutManager.addScout(bw::UnitTypes::Protoss_Probe);
+    }
 
-            // Otherwise, try to build enough buildings up to the desired quota.
-            while (progress < item.count && m_productionManager.addBuildRequest(item.type)) {
-                progress++;
-            }
-            break;
-        }
+    // If we have a sufficient number of workers, we can start expanding our army. We
+    // choose the number of gateways to build based on the number of fighting units we
+    // currently have so we don't run into bottlenecks in army production.
+    if (workerCount >= 11) {
+        m_productionManager.targetBuildRequests(
+            bw::UnitTypes::Protoss_Gateway, fighterCount / 5 + 1);
+    }
+}
 
-        case ActionType::TRAIN:
-        {
-            // If the number of fully trained units meets the quota, then we're done.
-            int current = m_unitManager.selfCount(
-                bw::Filter::GetType == item.type && bw::Filter::IsCompleted);
-            int progress = m_unitManager.selfCount(bw::Filter::GetType == item.type);
+void StrategyManager::onTerranFrame() {
+    // Terran strategy is almost identical to Protoss strategy, but given the faster
+    // building speed, some of the constants have been tweaked for somewhat more optimal
+    // resource usage.
+    m_productionManager.idleTrainRequests(bw::UnitTypes::Terran_SCV, false);
+    m_productionManager.idleTrainRequests(bw::UnitTypes::Terran_Marine, false);
 
-            m_completion[i] = current >= item.count;
+    m_productionManager.targetBuildRequests(
+        bw::UnitTypes::Terran_Supply_Depot, g_self->supplyUsed() / 12);
 
-            // Otherwise, try to train enough units up to the desired quota.
-            while (progress < item.count && m_buildingManager.addTrainRequest(item.type)) {
-                progress++;
-            }
-            break;
-        }
+    int workerCount = m_unitManager.selfCount(bw::GetType == bw::UnitTypes::Terran_SCV);
+    int fighterCount = m_unitManager.selfCount(bw::GetType == bw::UnitTypes::Terran_Marine);
 
-        case ActionType::MORPH:
-        {
-            // If the number of fully morphed units meets the quota, then we're done.
-            int current = m_unitManager.selfCount(bw::Filter::GetType == item.type);
-            int progress = m_unitManager.selfCount(
-                bw::Filter::GetType == item.type || bw::Filter::BuildType == item.type);
+    if (workerCount >= 7 && m_scoutManager.countScouts(bw::UnitTypes::Terran_SCV) == 0) {
+        m_scoutManager.addScout(bw::UnitTypes::Terran_SCV);
+    }
 
-            m_completion[i] = current >= item.count;
+    if (workerCount >= 10) {
+        m_productionManager.targetBuildRequests(
+            bw::UnitTypes::Terran_Barracks, fighterCount / 6 + 1);
+    }
+}
 
-            // Otherwise, try to morph enough units up to the desired quota.
-            while (progress < item.count && m_productionManager.addMorphRequest(item.type)) {
-                progress++;
-            }
-            break;
-        }
+void StrategyManager::onZergFrame() {
+    // Before we morph all the larva we have this frame, check to see if we need an
+    // overlord. We always want a nice slack in our supply amount, especially because of
+    // the fast morph time of Zerg units.
+    m_productionManager.targetTrainRequests(
+        bw::UnitTypes::Zerg_Overlord, g_self->supplyUsed() / 12 + 1, true);
 
-        case ActionType::SCOUT:
-        {
-            // If we currently have the required number of scouts, then we're done.
-            int current = m_scoutManager.countScouts(item.type);
+    // The rest of our larva can be spent on morphing new workers and fighters.
+    m_productionManager.groupTrainRequests({
+        {bw::UnitTypes::Zerg_Drone,    0.40},
+        {bw::UnitTypes::Zerg_Zergling, 0.60},
+    }, true);
 
-            m_completion[i] = current >= item.count;
+    int workerCount = m_unitManager.selfCount(bw::GetType == bw::UnitTypes::Zerg_Drone);
+    int fighterCount = m_unitManager.selfCount(bw::GetType == bw::UnitTypes::Zerg_Broodling);
 
-            // Otherwise, add more scouts to fill up the quota of scouts in action.
-            while (current < item.count && m_scoutManager.addScout(item.type)) {
-                current++;
-            }
-            break;
-        }
+    // If we have enough workers past a certain threshold, we can make a spawning pool in
+    // order to gain the ability to morph fighter units.
+    if (workerCount >= 11) {
+        m_productionManager.targetBuildRequests(bw::UnitTypes::Zerg_Spawning_Pool, 1);
+    }
 
-        case ActionType::ATTACK:
-        {
-            // If the attacking prerequisite has been satisfied, then we instruct the
-            // combat manager to attack. For now, once the attack has started, it cannot
-            // be stopped, so this action is permanently completed.
-            if (!m_completion[i]) {
-                m_combatManager.attack();
-                m_completion[i] = true;
-            }
-            break;
-        }
-        }
+    // We want to make sure our production of new units always stays fast, so we build
+    // extra hatcheries whenever we exceed a certain number of units.
+    m_productionManager.targetBuildRequests(
+        bw::UnitTypes::Zerg_Hatchery, (workerCount + fighterCount) / 15 + 1);
+
+    // Like the other races, send a scout once we have a decent number of workers.
+    if (workerCount >= 9 && m_scoutManager.countScouts(bw::UnitTypes::Zerg_Drone) == 0) {
+        m_scoutManager.addScout(bw::UnitTypes::Zerg_Drone);
     }
 }
