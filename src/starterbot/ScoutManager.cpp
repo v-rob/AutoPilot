510b2caeb137a884a5e6ad809b6d3f9317136569
HenrySchiffUP <schiff26@up.edu>
2025-04-12 16:29:43 -0700

    Merge

--- src/starterbot/ScoutManager.cpp
+++ src/starterbot/ScoutManager.cpp
@@ -50,70 +50,22 @@ void ScoutManager::onStart() {
         }
     }
 
-<<<<<<< HEAD
     // If position is not our base, add it to the list of startingLocations the Scout should search.
     for (bw::TilePosition spawnPos : g_game->getStartLocations()) {
-        if (spawnPos.x != myBase.x and spawnPos.y != myBase.y){
+        if (spawnPos.x != myBase.x and spawnPos.y != myBase.y) {
             startingLocations.push_back(spawnPos);
-            std::cout << spawnPos << ""; 
+            std::cout << spawnPos << "";
         }
     }
-=======
-    //std::cout << "This is where my base is at: " << myBase << "\n";
-
-
-    //std::cout << "And here's the list of the possible places that our scout should move: " << "";
-    //for (bw::TilePosition spawnPos : g_game->getStartLocations()) {
-    //    //if the pos is not my base, add it to the starting locations that the scout should 
-    //    if (spawnPos.x != myBase.x and spawnPos.y != myBase.y){
-    //        startingLocations.push_back(spawnPos);
-    //        std::cout << spawnPos << ""; 
-    //        //g_game->
-    //    }
-    //}
->>>>>>> 577bd5be08724953be0a5f7cc1dfee96bdf516fa
-
-    //std::cout << "\n " << "";
 }
 
 void ScoutManager::onFrame() {
-<<<<<<< HEAD
-    /* 
+    /*
     *  For now, scouting behavior is more complex. The scout will go through all spawn locations
     *  besides our location. When it finds the enemy base, use VectorField to retreive
     *  the direction of the vector at the Scout's current Position. The scout's movement is
     *  dependent on where the Vector is pointing at the specific Scout Position.
     */
-=======
-    // For now, scouting behavior is very simplistic. Scouts are simply sent to each
-    // unexplored potential start location in order to find the enemy's location. They
-    // will not patrol the area any further after doing so.
-
-    bw::TilePosition firstPos;
-    bw::TilePosition secondPos;
-    bw::TilePosition middlePos;
-
-    //std::cout << "Finish Search Enemy Base? " << finishSearchEnemyBase << "\n"; //once when reached to spawn loc, set to 1, incorrect
-
-
-
-    if (finishSearchEnemyBase == true){
-        //REPLACE THIS WITH THE POINTS FROM THE VECTOR FIELD
-      //std::cout << "[" << enemyBasePos.x << ", " << enemyBasePos.y << "]";
-        if (enemyBasePos.y <= 65){//this means if the enemy base is on the top half
-            firstPos = bw::TilePosition(enemyBasePos.x + 6, enemyBasePos.y +6);
-            secondPos = bw::TilePosition(enemyBasePos.x - 6, enemyBasePos.y +6);
-            middlePos = bw::TilePosition(enemyBasePos.x, enemyBasePos.y +9);
-        }
-        if (enemyBasePos.y > 65){//if enemy base is on bottom half
-            firstPos = bw::TilePosition(enemyBasePos.x + 6, enemyBasePos.y - 6);
-            secondPos = bw::TilePosition(enemyBasePos.x - 6, enemyBasePos.y - 6);
-            middlePos = bw::TilePosition(enemyBasePos.x, enemyBasePos.y - 9);
-        }
-    }
-
->>>>>>> 577bd5be08724953be0a5f7cc1dfee96bdf516fa
-
 
     for (bw::Unit scout : m_scouts) {
         // We will use the vectors for maneuvering.
